14b34b62fb86d006edfb06df4a6d2394663446f4
gzylll <1165038963@qq.com>
2017-06-22 10:53:04 +0800

    优化-3-5

--- app/src/main/java/valderfields/rjb_1/Presenter/LoginPresenter.java
+++ app/src/main/java/valderfields/rjb_1/Presenter/LoginPresenter.java
@@ -14,6 +14,7 @@ import cn.smssdk.SMSSDK;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.FormBody;
+import okhttp3.MediaType;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
@@ -156,4 +157,5 @@ public class LoginPresenter extends Observable{
             }
         }.start();
     }
+
 }
